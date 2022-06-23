1ff869a74b52b701073b4a2855e6523ed77a11f4
DESKTOP-05Q6GBI\Nadine <wi21b045@technikum-wien.at>
2022-06-23 11:00:39 +0200

    Initial commit

---
+++ src/main/java/at/technikum/maintenancemonitor/MaintenanceMonitorApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.maintenancemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaintenanceMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/technikum/maintenancemonitor/MaintenanceMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.maintenancemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
